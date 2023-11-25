dd4cc12b87a492af7fab0eefbb0d1c870cdea7de
Mohamad Ripal Perdiansyah <mohamadripal001@gmail.com>
2023-11-25 08:16:13 +0700

    :sparkles: Initial Commit NyokLapor Project

---
+++ README.md
@@ -0,0 +1,7 @@
+# NyokLapor
+
+<br />
+<p align="center">
+  <img src="./src/public/icons/nyoklapor-icon.png" alt="Logo" width="150" height="100">
+  <h3 align="center">NyokLapor</h3>
+</p>
---
+++ src/scripts/index.js
@@ -0,0 +1,8 @@
+import '../styles/main.css';
+
+const container = document.querySelector('body');
+container.innerHTML = `
+    <h1 class="text-3xl font-bold underline">
+        NyokLapor
+    </h1>
+`;
