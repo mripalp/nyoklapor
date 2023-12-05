f1486c22398bd87cee36e4004ef13983402bd5fc
Mohamad Ripal Perdiansyah <94633492+mripalp@users.noreply.github.com>
2023-12-05 10:11:28 +0700

    feat: :white_check_mark: Menambahkan routes setiap halaman di admin

---
+++ src/scripts/routes/routes.js
@@ -0,0 +1,11 @@
+import AccountPage from '../views/pages/admin/account';
+import Admin from '../views/pages/admin/admin';
+import ReportPage from '../views/pages/admin/total-report';
+
+const routes = {
+  '/admin': Admin,
+  '/account': AccountPage,
+  '/report': ReportPage,
+};
+
+export default routes;
